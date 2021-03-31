3413a7c81994db0103dd5440e9fb1967ccd920fd
Joanna <jlazartelagos@gmail.com>
2021-03-31 00:47:57 -0300

    completo

--- anio-nuevo/index.js
+++ anio-nuevo/index.js
@@ -1,7 +1,7 @@
 /*
 Crear una función esAnioNuevo que tome como 
 argumento un string fecha con el formato 
-DD:MM:YYYY un argumento y devuelva true si la 
+DD/MM/YYYY un argumento y devuelva true si la 
 fecha es año nuevo o false si no lo es
 
 esAnioNuevo('03/05/2015') // false
@@ -9,5 +9,14 @@ esAnioNuevo('22/01/1987') // false
 esAnioNuevo('01/01/2020') // true
 */
 const esAnioNuevo = (string) => {
-    const arr = string.split("");
-}
+    const fecha = string.split("/");
+    console.log(fecha);
+    const dia = Number(fecha[0]);
+    const mes = Number(fecha[1]);
+    const anio = Number(fecha[2]);
+    return dia == 1 && mes == 1 && anio >= 0 && anio < 2021;
+
+};
+console.log(esAnioNuevo('03/05/2015'));// false
+console.log(esAnioNuevo('22/01/1987')); // false
+console.log(esAnioNuevo('01/01/2020')); // true
--- duracion-en-seg/index.js
+++ duracion-en-seg/index.js
@@ -1,13 +1,24 @@
 /*
 Crear una función obtenerDuracionEnSegundos que 
 tome como argumento un string duracion con el 
-formato (mm:ss) y la cantidad de segundos totales 
-de la duración
+formato (mm:ss) y devuelva la cantidad de segundos 
+totales de la duración
 
 obtenerDuracionEnSegundos('00:33') // 33
 obtenerDuracionEnSegundos('01:05') // 65
 obtenerDuracionEnSegundos('10:42') // 642
 */
-const obtenerDuracionEnSegundos = (string) => {
-    
-}
+const obtenerDuracionEnSegundos = (duracion) => {
+    const minutosYsegundos = duracion.split(":");
+    console.log(minutosYsegundos);
+    const minutos = Number(minutosYsegundos[0]);
+    const segundos = Number(minutosYsegundos[1]);
+    if (minutos >= 1) {
+        return minutos * 60 + segundos;
+    } else {
+        return segundos;
+    }
+};
+console.log(obtenerDuracionEnSegundos('00:33')); // 33
+console.log(obtenerDuracionEnSegundos('01:05')); // 65
+console.log(obtenerDuracionEnSegundos('10:42')); // 642
